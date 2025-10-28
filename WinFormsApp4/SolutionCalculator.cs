0ba65cf883d9984f3bc05cf20da6b85137c1a632
Caera0777 <vanya.shuga@gmail.com>
2025-10-28 19:35:46 +0100

    Dodano klasę SolutionCalculator z logiką obliczeń roztworu

---
+++ WinFormsApp4/SolutionCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsApp4
+{
+    public class SolutionCalculator
+    {
+        public double Pojemnosc { get; set; }
+        public double Procent { get; set; }
+        public int LiczbaNaczyn { get; set; }
+
+        public SolutionCalculator(double pojemnosc, double procent, int liczbaNaczyn)
+        {
+            Pojemnosc = pojemnosc;
+            Procent = procent;
+            LiczbaNaczyn = liczbaNaczyn;
+        }
+
+        public double ObliczLacznaObjetosc()
+        {
+            return Pojemnosc * LiczbaNaczyn;
+        }
+
+        public double ObliczCzystaSubstancje()
+        {
+            return ObliczLacznaObjetosc() * (Procent / 100.0);
+        }
+    }
+}
