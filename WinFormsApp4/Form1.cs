21e00c7939240f04a42abce82448bf342258a96e
Caera0777 <vanya.shuga@gmail.com>
2025-10-28 19:36:04 +0100

    Dodano obsługę przycisku w Form1 i połączenie z klasą SolutionCalculator

--- WinFormsApp4/Form1.cs
+++ WinFormsApp4/Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace WinFormsApp4
 {
     public partial class Form1 : Form
@@ -5,6 +8,28 @@ namespace WinFormsApp4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonOblicz_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double pojemnosc = double.Parse(textBoxPojemnosc.Text);
+                double procent = double.Parse(textBoxProcent.Text);
+                int liczba = int.Parse(textBoxLiczba.Text);
+
+                SolutionCalculator calc = new SolutionCalculator(pojemnosc, procent, liczba);
+
+                double laczna = calc.ObliczLacznaObjetosc();
+                double czysta = calc.ObliczCzystaSubstancje();
+
+                textBoxWynik.Text = $"??czna obj?to?? p?ynu: {laczna} ml\n" +
+                                    $"Obj?to?? czystej substancji: {czysta:F2} ml";
+            }
+            catch
+            {
+                MessageBox.Show("Wprowad? poprawne dane liczbowe!", "B??d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
